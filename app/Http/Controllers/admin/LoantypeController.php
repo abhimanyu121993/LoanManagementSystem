d9eb930ebb2cae65a72684c0c677ad7597864903
Satyam Shukla <shuklasatyam23056@gmail.com>
2023-04-24 12:36:17 +0530

    loan type

--- app/Http/Controllers/admin/LoantypeController.php
+++ app/Http/Controllers/admin/LoantypeController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Error;
+use App\Models\LoanType;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class LoantypeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.loan_type');
+        $loan_types = LoanType::all();
+        return view('admin.loan_type',compact('loan_types'));
     }
 
     /**
@@ -28,15 +33,28 @@ class LoantypeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request ->validate([
+            'loan_type' => 'required',
+        ]);
+        LoanType::create([
+            'loan_type' => $request->loan_type,
+        ]);
+        return redirect()->back()->with('toast_success', 'Loan Type Added Successfully !');
     }
-
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        try {
+            LoanType::destroy($id);
+            return redirect()->back()->with('toast_success', 'Loan type deleted succesfully!');
+        } catch (Exception $ex) {
+
+            $url = URL::current();
+            Error::create(['url' => $url, 'message' => $ex->getMessage()]);
+            return redirect()->back()->with('toast_error', 'Something Went Wrong!');
+        }
     }
 
     /**
@@ -44,7 +62,8 @@ class LoantypeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+       $data = LoanType::find($id);
+       return view('admin.loan_type',compact('data'));
     }
 
     /**
@@ -52,14 +71,30 @@ class LoantypeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request ->validate([
+            'loan_type' => 'required',
+        ]);
+        LoanType::find($id)->update([
+            'loan_type' => $request->loan_type,
+        ]);
+        return redirect()->route('admin.loan-type.index')->with('toast_success', 'Loan Type updated Successfully !');
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
+    {   
+      //
+    }
+
+    public function status($id)
     {
-        //
+       $data = LoanType::find($id)->status;
+       LoanType::find($id)->update([
+        'status'=> $data == 1?0:1
+       ]);
+
+       return "status updated successfully!";
     }
 }
--- app/Models/LoanType.php
+++ app/Models/LoanType.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LoanType extends Model
 {
     use HasFactory,SoftDeletes;
+    
+    protected $fillable=[
+        'loan_type',
+        'status',
+    ];
 }
